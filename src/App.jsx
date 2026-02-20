1156b9cff5ee7a5c5dbf9667f406589c64833676
dev-oriel <kiplangatoriel@gmail.com>
2026-02-20 19:13:51 +0300

    added the scroll functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import ScrollToTop from "./components/ScrollToTop";
 import LandingPage from "./pages/LandingPage";
 import Services from "./pages/Services";
 import Process from "./pages/Process";
@@ -12,17 +13,21 @@ import Contact from "./pages/Contact";
 function App() {
   return (
     <Router>
+      <ScrollToTop /> {/* Needs to be inside Router, but outside Routes */}
       <Layout>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/process" element={<Process />} />
-          <Route path="/start" element={<ProjectIntake />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        {/* Added the page-transition class here for a smooth fade-in */}
+        <div className="page-transition">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/process" element={<Process />} />
+            <Route path="/start" element={<ProjectIntake />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/faq" element={<FAQ />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
       </Layout>
     </Router>
   );
--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -5,11 +5,10 @@ export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    // Instantly scroll to the top left of the window whenever the path changes
     window.scrollTo({
       top: 0,
       left: 0,
-      behavior: "instant", // "instant" prevents the jarring visual of scrolling rapidly up the page
+      behavior: "smooth",
     });
   }, [pathname]);
 
